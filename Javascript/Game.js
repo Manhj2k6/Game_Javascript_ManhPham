6da1a3a98891b34dfaabc7b6a0fb15582e9d7bb8
Manhj2k6 <top1manh2k6@gmail.com>
2023-01-09 10:05:56 +0700

    Up code new

--- Javascript/Game.js
+++ Javascript/Game.js
@@ -2,11 +2,11 @@ var ManhPham = prompt('Chọn Búa, Kéo hay Lá');
 
 console.log("Người chới:", ManhPham)
 var random = Math.ceil(Math.random() * 20)
-if (random === 1 || random === 3 || random === 7 || random === 10 || random === 13 || random === 16 || random === 19) {
+if (random === 1 || random === 4 || random === 7 || random === 10 || random === 13 || random === 16 || random === 19) {
     random = 'Búa';
 }else if (random === 2 || random === 5 || random === 8 || random === 11 || random === 14 || random === 17 || random === 20) {
     random = 'Kéo';
-}else if (random === 4 || random === 6 || random === 9 || random === 12 || random === 15 || random === 18 || random === 21) {
+}else if (random === 3 || random === 6 || random === 9 || random === 12 || random === 15 || random === 18 || random === 21) {
     random = 'Lá'
 }
 console.log("Máy tính:", random)
